536115c3ccc2ac50f5043a205a938797296eb292
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-09-24 23:02:57 +0800

    :construction: 获取网页授权access_token

---
+++ src/main/java/org/springframework/security/oauth2/server/authorization/exception/AppidWeChatOffiaccountException.java
@@ -0,0 +1,15 @@
+package org.springframework.security.oauth2.server.authorization.exception;
+
+/**
+ * 微信公众号 AppID(公众号ID) 异常
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+public class AppidWeChatOffiaccountException extends WeChatOffiaccountException {
+
+    public AppidWeChatOffiaccountException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/org/springframework/security/oauth2/server/authorization/exception/SecretWeChatOffiaccountException.java
@@ -0,0 +1,15 @@
+package org.springframework.security.oauth2.server.authorization.exception;
+
+/**
+ * 微信公众号 Secret 异常
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+public class SecretWeChatOffiaccountException extends WeChatOffiaccountException {
+
+    public SecretWeChatOffiaccountException(String message) {
+        super(message);
+    }
+
+}
