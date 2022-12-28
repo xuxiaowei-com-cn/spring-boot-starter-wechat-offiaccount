ce7364064b7abec0dcf22cc930874f84fdb0409b
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-12-28 09:15:11 +0800

    :sparkles: 反序列化时忽略不存在的字段

--- src/main/java/org/springframework/security/oauth2/server/authorization/client/InMemoryWeChatOffiaccountService.java
+++ src/main/java/org/springframework/security/oauth2/server/authorization/client/InMemoryWeChatOffiaccountService.java
@@ -9,9 +9,9 @@ package org.springframework.security.oauth2.server.authorization.client;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,6 +21,7 @@ package org.springframework.security.oauth2.server.authorization.client;
  */
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -164,6 +165,7 @@ public class InMemoryWeChatOffiaccountService implements WeChatOffiaccountServic
 
 		WeChatOffiaccountTokenResponse weChatOffiaccountTokenResponse;
 		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		try {
 			weChatOffiaccountTokenResponse = objectMapper.readValue(forObject, WeChatOffiaccountTokenResponse.class);
 		}
