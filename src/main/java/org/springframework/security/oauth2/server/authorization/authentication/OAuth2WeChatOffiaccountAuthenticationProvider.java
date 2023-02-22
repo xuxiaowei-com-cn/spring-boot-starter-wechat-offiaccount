040c72616f0859ee226fb6f82b4a09b9431841f3
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-02-22 08:49:52 +0800

    :arrow_up: spring-authorization-server 0.4.1、spring-javaformat 0.0.36

--- src/main/java/org/springframework/security/oauth2/server/authorization/authentication/OAuth2WeChatOffiaccountAuthenticationProvider.java
+++ src/main/java/org/springframework/security/oauth2/server/authorization/authentication/OAuth2WeChatOffiaccountAuthenticationProvider.java
@@ -9,9 +9,9 @@ package org.springframework.security.oauth2.server.authorization.authentication;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -112,7 +112,7 @@ public class OAuth2WeChatOffiaccountAuthenticationProvider implements Authentica
 		Set<String> requestedScopes = StringUtils.commaDelimitedListToSet(grantAuthenticationToken.getScope());
 
 		OAuth2ClientAuthenticationToken clientPrincipal = OAuth2AuthenticationProviderUtils
-				.getAuthenticatedClientElseThrowInvalidClient(grantAuthenticationToken);
+			.getAuthenticatedClientElseThrowInvalidClient(grantAuthenticationToken);
 		RegisteredClient registeredClient = clientPrincipal.getRegisteredClient();
 
 		// 自定义微信公众号用户的IP与SessionId
@@ -140,7 +140,7 @@ public class OAuth2WeChatOffiaccountAuthenticationProvider implements Authentica
 		}
 
 		WeChatOffiaccountTokenResponse weChatOffiaccountTokenResponse = weChatOffiaccountService
-				.getAccessTokenResponse(appid, code, ACCESS_TOKEN_URL);
+			.getAccessTokenResponse(appid, code, ACCESS_TOKEN_URL);
 
 		String openid = weChatOffiaccountTokenResponse.getOpenid();
 		String unionid = weChatOffiaccountTokenResponse.getUnionid();
--- src/main/java/org/springframework/security/oauth2/server/authorization/http/WeChatOffiaccountCodeHttpFilter.java
+++ src/main/java/org/springframework/security/oauth2/server/authorization/http/WeChatOffiaccountCodeHttpFilter.java
@@ -9,9 +9,9 @@ package org.springframework.security.oauth2.server.authorization.http;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -98,7 +98,7 @@ public class WeChatOffiaccountCodeHttpFilter extends HttpFilter {
 			String grantType = WECHAT_OFFIACCOUNT.getValue();
 
 			WeChatOffiaccountProperties.WeChatOffiaccount offiaccount = weChatOffiaccountService
-					.getWeChatOffiaccountByAppid(appid);
+				.getWeChatOffiaccountByAppid(appid);
 			String clientId = offiaccount.getClientId();
 			String clientSecret = offiaccount.getClientSecret();
 			String tokenUrlPrefix = offiaccount.getTokenUrlPrefix();
@@ -119,7 +119,7 @@ public class WeChatOffiaccountCodeHttpFilter extends HttpFilter {
 			uriVariables.put(OAuth2WeChatOffiaccountParameterNames.SESSION_ID, session == null ? "" : session.getId());
 
 			OAuth2AccessTokenResponse oauth2AccessTokenResponse = weChatOffiaccountService
-					.getOAuth2AccessTokenResponse(request, response, tokenUrlPrefix, TOKEN_URL, uriVariables);
+				.getOAuth2AccessTokenResponse(request, response, tokenUrlPrefix, TOKEN_URL, uriVariables);
 			if (oauth2AccessTokenResponse == null) {
 				return;
 			}
